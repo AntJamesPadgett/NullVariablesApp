db99cd12b3768f2ae5d9d43a7cd6dd534175272b
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-27 13:12:20 -0600

    completed work

---
+++ NullVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//null- lack of
+//haven't asked for the age yet
+
+//int? age = null;
+bool? birthday = null;
+double? battingAverage = null;
+string firstName = null;
+
+// We have asked for the age now
+
+
+
+int? age = null;
